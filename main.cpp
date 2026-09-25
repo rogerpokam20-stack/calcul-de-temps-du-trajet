53422534889da13e269d53e7e55e342309bcee15
rogerpokam20-stack <rogerpokam20@gmail.com>
2026-09-25 14:46:44 +0200

    pseudo code calcul de temp de trajet et code C++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-/* --------------------------- 
-Laboratoire : 02
-Auteur(s) : 
-Date : 
-But : Calcul du temps de trajet 
-Remarque(s) : 
---------------------------- */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    double L1=6;
+    double Dy=10-L1;
+    double Dx=3;
+    double V1=5;
+    double V2=2;
+    double L2=sqrt(Dy*Dy+Dx*Dx);
+    double T1=L1/V1;
+    double T2=L2/V2;
+    double TT=T1+T2;
+    cout<<TT<<endl;
+
+    return 0;
+
+}
